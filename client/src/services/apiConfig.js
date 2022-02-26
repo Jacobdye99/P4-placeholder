e2c7e780f5e49fd0386254671e2f25bc890900ca
Jacob Dye <jacobdye@pop-os.localdomain>
2022-02-26 17:07:09 -0500

    Working on api

---
+++ client/src/services/apiConfig.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const KEY = process.env.KEY
+
+const rubyUrl = 'http://localhost:3000'
+
+const omdbUrl = `http://www.omdbapi.com/?apikey=${KEY}&`
+
+export const api = axios.create({
+    baseURL: rubyUrl
+})
+
+export const movieApi = axios.create({
+    baseURL: omdbUrl
+})
---
+++ client/src/services/users.js
@@ -0,0 +1,24 @@
+import { api } from "./apiConfig";
+
+export const loginUser = async (loginData) => {
+    const res = await api.post('/auth/login', {authentication: loginData})
+    localStorage.setItem('authToken', res.data.token)
+    api.defaults.headers.common.authorization = `Bearer ${res.data.token}`
+    return res.data.user
+}
+
+export const registerUser = async (registerData) => {
+    const res = await api.post('/users', {user: registerData})
+    localStorage.setItem('authToken', res.data.token)
+    api.defaults.headers.common.authorization = `Bearer ${res.data.token}`
+    return res.data.user
+}
+export const verifyUser = async () => {
+    const token = localStorage.getItem('authToken')
+    if (token) {
+        api.defaults.headers.common.authorization = `Bearer ${token}`
+        const res = await api.get('/auth/verify')
+        return res.data
+    }
+    return false
+}
