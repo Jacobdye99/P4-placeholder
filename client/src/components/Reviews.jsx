556932167e38c1efe8e5960bdfa4a50241c9e38b
Jacob Dye <jacobdye@pop-os.localdomain>
2022-02-27 22:20:48 -0500

    Added Reviews Feed

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -6,10 +6,13 @@ export default function Reviews(props) {
     <div>
         {props.reviews.map(review => {
             return (
-            <Link key={review.id} to={`/reviews/${review.id}`}>
+                <div>
                 <h3>{review.user.username}</h3>
+            <Link key={review.id} to={`/reviews/${review.id}`}>
                 <h4>{review.review}</h4>
             </Link>
+                <h5>{review.likes}</h5>
+                </div>
         )})}
     </div>
   )
