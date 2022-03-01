8aeeac258430803aad7692cee978ee1b39b1f13e
Jacob Dye <jacobdye@pop-os.localdomain>
2022-02-28 21:04:56 -0500

    Fixed stars for edit

--- client/src/components/EditReview/EditReview.jsx
+++ client/src/components/EditReview/EditReview.jsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import StarRating2 from '../StarRating/StarRating'
+import StarRating from '../StarRating/StarRating'
 
 export default function EditReview(props) {
 
-    const [review, setReview] = useState('')
+    // const [review, setReview] = useState('')
     const [stars, setStars] = useState(0)
-    const [likes, setLikes] = useState(0)
+    // const [likes, setLikes] = useState(0)
     const [movieId, setMovieId] = useState("")
-    const [data, setData] = useState()
+    const review = {
+        review: "",
+        likes: 0,
+        stars: 0,
+        // eslint-disable-next-line no-use-before-define
+        movie_id: {movieId}
+      }
+    const [data, setData] = useState(review)
     const {id} = useParams()
     const navigate = useNavigate()
     
@@ -18,30 +25,41 @@ export default function EditReview(props) {
             return review.id === parseInt(id)
         })
         if (foundReview) {
-            setReview(foundReview.review)
+            // setReview(foundReview.review)
             setStars(foundReview.stars)
-            setLikes(foundReview.likes)
+            // setLikes(foundReview.likes)
             setMovieId(foundReview.movie_id)
             setData(foundReview)
         }
        
     }, [id, props.reviews])
+    const handleTextInput = (e) => {
+        e.preventDefault()
+        const {id, value} = e.target;
+        setData((prevData) => ({
+            ...prevData,
+            [id]: value
+        }))
+        console.log(data)
+      }
+      console.log(data.movie_id)
 
   return (
     <form onSubmit={(e) => {
         e.preventDefault()
-        const newReview = {
-            review,
-            stars,
-            likes,
-            movieId
-        }
-        navigate('/reviews')
-        props.handleEdit(id, newReview)
+        // const newReview = {
+        //     review,
+        //     stars,
+        //     likes,
+        //     movieId
+        // }
+        // navigate('/reviews')
+        props.handleEdit(id, data)
+        console.log(stars)
     }}>
-      <textarea type='text' id='review' onChange={(e) => setReview(e.target.value)}  />
-      <StarRating2 stars={stars} setStars={setStars}/>
-      <button type='submit'>Edit</button>
+      <textarea type='text' id='review' onChange={handleTextInput}  />
+      <StarRating handleTextInput={handleTextInput} stars={data.stars}/>
+      <button type='submit'>Post</button>
     </form>
   )
 }
--- client/src/components/StarRating/StarRating2.jsx
+++ client/src/components/StarRating/StarRating2.jsx
@@ -9,7 +9,9 @@ export default function StarRating(props) {
         {[...Array(5)].map((star, i) => {
             const ratingValue = i + 1;
             return  <label key={i}>
-                            <input type='radio' name="rating" value={ratingValue} id="stars" onClick={(e) => props.setStars(e.target.value)} />
+                            <input type='radio' name="rating" value={ratingValue} id="stars" onClick={(e) => {
+                              console.log(ratingValue)
+                              props.setStars(e.target.value)}} />
                             <FaStar className='star' color={ratingValue <= ( hover || props.stars) ? "#ffc107" : "#e4e5e9"} size={50} onMouseEnter={() => setHover(ratingValue)} onMouseLeave={() => setHover(null)} />
                     </label>
         })}
