cdb149e4347f5e0c925fae18c2f90c307aad34c0
Jacob Dye <jacobdye@pop-os.localdomain>
2022-03-02 09:32:33 -0500

    Fixing http error

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const rubyUrl = process.env.NODE_ENV === 'production' ? /* link to your heroku app. Example:*/'https://reviewiop4.herokuapp.com/' : 'http://localhost:3000'
 
-const omdbUrl = `http://www.omdbapi.com`
+const omdbUrl = `https://www.omdbapi.com`
 
 export const api = axios.create({
     baseURL: rubyUrl
