b1568c1d7270b8e2f1a2daafe49f859b85e1e6bd
Jacob Dye <jacobdye@pop-os.localdomain>
2022-03-01 21:50:41 -0500

    Finished Footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,11 @@
 import { RiGithubLine } from 'react-icons/ri'
+import { BsLinkedin } from 'react-icons/bs'
+import './Footer.css'
 export default function Footer() {
     return (
-        <div>
-            <a target="_blank" href="https://github.com/Jacobdye99" rel="noreferrer"><RiGithubLine /></a>
-            <a target="_blank"  href="https://www.linkedin.com/in/jacob-dye-55659b149/" rel="noreferrer">Linkedin</a>
+        <div className='Footer'>
+            <a target="_blank" href="https://github.com/Jacobdye99" rel="noreferrer" className='github'><RiGithubLine className='github' size={40}/></a>
+            <a target="_blank"  href="https://www.linkedin.com/in/jacob-dye-55659b149/" rel="noreferrer" className='linkedin'><BsLinkedin className='linkedin' size={40}/></a>
         </div>
     )
 }
